c1958b7b9ce9ab9a4cdb02fe94cee2aaabf9946b
redraiment <redraiment@gmail.com>
2022-08-11 00:31:27 +0800

    Setting maven & clojars mirrors

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,8 @@
-{:repl {:plugins [[cider/cider-nrepl "0.28.5"]
-                  [mx.cider/enrich-classpath "1.9.0"]]}}
+{:user {:mirrors {"central" {:name "aliyun"
+                             :url "https://maven.aliyun.com/repository/public"}
+                  "clojars" {:name "tsinghua"
+                             :url "https://mirrors.tuna.tsinghua.edu.cn/clojars/"}}}
+ :repl {:plugins [[cider/cider-nrepl "0.28.4"
+                   :exclusions [org.clojure/clojure]]
+                  [mx.cider/enrich-classpath "1.9.0"
+                   :exclusions [org.clojure/clojure]]]}}
