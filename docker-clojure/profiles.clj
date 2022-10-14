d901e5b9ce7b5b93bd7398389b261081e6523309
redraiment <redraiment@gmail.com>
2022-10-14 23:41:52 +0800

    build: update to 2022-10-14
    * Add maven central repo for leiningen.

--- docker-clojure/profiles.clj
+++ docker-clojure/profiles.clj
@@ -1,7 +1,9 @@
-{:user {:mirrors {"central" {:name "aliyun"
-                             :url "https://maven.aliyun.com/repository/public"}
+{:user {:mirrors {"aliyun" {:name "aliyun"
+                            :url "https://maven.aliyun.com/repository/public"}
                   "clojars" {:name "tsinghua"
-                             :url "https://mirrors.tuna.tsinghua.edu.cn/clojars/"}}}
+                             :url "https://mirrors.tuna.tsinghua.edu.cn/clojars/"}
+                  "central" {:name "maven central"
+                             :url "https://repo.maven.apache.org/maven2"}}}
  :repl {:plugins [[cider/cider-nrepl "0.28.4"
                    :exclusions [org.clojure/clojure]]
                   [mx.cider/enrich-classpath "1.9.0"
